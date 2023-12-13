dcb901024747d2d908682383790eb1a6c9b31263
FabulousBastard <dorian.vidovic1@gmail.com>
2023-12-13 16:18:15 +0100

    zadaca 3 zadatak 1 100%good

--- 3/zadatak1.c
+++ 3/zadatak1.c
@@ -4,40 +4,52 @@ ZADATAK 1. Napisati program koji unosi cijele brojeve sve dok se četiri puta za
 */
 
 #include <stdio.h>
-//#include <string.h> // za memset
 
 int main()
 {
+	int uneseniBrojevi[1000] = {0};
+
 	int kolBrVecihOdDeset = 0;
+	int kolBr = 0;
+	
 	int br = 0;
+
 	int najCesciBr = 0;
 	int max = 0;
-	int uneseniBrojevi[2000] = {0};
+	int brojilo=0;
 
-	//memset(uneseniBrojevi, 0, sizeof(uneseniBrojevi)); 
 
 	while( kolBrVecihOdDeset < 4 ){
 
 		printf("Unesi broj: ");
 		scanf("%d", &br);
-		uneseniBrojevi[br]++;
+		uneseniBrojevi[kolBr] = br;
+
 
 		if(br > 10){
 		    kolBrVecihOdDeset++;
 		}else{
 			kolBrVecihOdDeset = 0;
 		}
+		kolBr++;
 	}
-	
-	for (int i = 0; i < 99; ++i)
+	for (int i = 0; i < kolBr; ++i)
+	{
+		for (int j = 0; j < kolBr; ++j)
 		{
-			if (uneseniBrojevi[i] > max)
+			if (uneseniBrojevi[i] == uneseniBrojevi[j])
 			{
-				najCesciBr = i;
-				max = uneseniBrojevi[i];
+				brojilo++;
+			}
+
+			if (brojilo > max)
+			{
+				max = brojilo;
+				najCesciBr = uneseniBrojevi[i];
 			}
 		}
-
+			brojilo = 0;
+	}
 
 	printf("Broj koji je unesen najvise puta je: %d\n", najCesciBr );
 	scanf("%d",&br);
